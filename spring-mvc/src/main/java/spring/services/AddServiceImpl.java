2a8b0d8774806d975c80b4027db92bcffa735a26
孙婷婷 <hzsuntingting@corp.netease.com>
2018-09-03 20:13:36 +0800

    调通spring-mvc,需要idea配置为web项目

--- spring-mvc/src/main/java/spring/services/AddServiceImpl.java
+++ spring-mvc/src/main/java/spring/services/AddServiceImpl.java
@@ -1,7 +1,6 @@
 package spring.services;
 
 import com.alibaba.dubbo.rpc.RpcContext;
-import demo.dubbo.api.DemoService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
 
@@ -17,15 +16,15 @@ public class AddServiceImpl implements AddService {
     }
 
 
-    public void test(){
-        ClassPathXmlApplicationContext context
-			= new ClassPathXmlApplicationContext("classpath:dubbo-consumer.xml");
-		context.start();
-
-		String username = "ting";
-		DemoService demoService = (DemoService) context.getBean("demoService");
-		RpcContext.getContext().setAttachment("invokeIds","1713,1714,1716,1717");
-		String result = demoService.changeUsername(username);
-		System.out.println(result);
-    }
+//    public void test(){
+//        ClassPathXmlApplicationContext context
+//			= new ClassPathXmlApplicationContext("classpath:dubbo-consumer.xml");
+//		context.start();
+//
+//		String username = "ting";
+//		DemoService demoService = (DemoService) context.getBean("demoService");
+//		RpcContext.getContext().setAttachment("invokeIds","1713,1714,1716,1717");
+//		String result = demoService.changeUsername(username);
+//		System.out.println(result);
+//    }
 }
